f49b735f4d4e879c2fb121a6f94f933ebd90d3f5
Ni3een <149294882+Ni3een@users.noreply.github.com>
2024-09-17 11:19:01 +0530

    Time: 124 ms (53.66%), Space: 75.1 MB (71.87%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        if (fruits.size() == 1) return 1;
-        if (fruits.size() == 2) return 2; // Return 2 regardless of whether the elements are the same or different
-        
-        int i = 0, j = 0, maxi = 1;
-        unordered_map<int, int> mp;
-        
-        while (j < fruits.size()) {
+        if(fruits.size()==1) return 1;
+        if(fruits.size()==2) return 2;
+        int i=0,j=0,maxi=1;
+        unordered_map<int,int> mp;
+        while(j<fruits.size()){
             mp[fruits[j]]++;
-            if (mp.size() <= 2) {
-                maxi = max(maxi, j - i + 1);
+            if(mp.size()<=2){
+                maxi=max(maxi,j-i+1);
                 ++j;
-            } else {
-                while (mp.size() > 2) {
+            }
+            else{
+                while(mp.size()>2){
                     mp[fruits[i]]--;
-                    if (mp[fruits[i]] == 0) {
-                        mp.erase(fruits[i]);
+                    if(mp[fruits[i]]==0){
+                    mp.erase(fruits[i]);
                     }
                     ++i;
                 }
@@ -25,4 +24,5 @@ public:
         }
         return maxi;
     }
+    
 };
