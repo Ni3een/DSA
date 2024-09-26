1d0dbc88c8ede2aceebf7b8fdf7edc71be4c49dd
Ni3een <149294882+Ni3een@users.noreply.github.com>
2024-09-26 21:01:39 +0530

    Time: 0 ms (100%), Space: 9.6 MB (25.33%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+       int n=stones.size();
+       priority_queue<int> pq;
+       for(int i=0;i<n;i++){
+        pq.push(stones[i]);
+       }
+       while(pq.size()>1){
+        int t1=pq.top();
+        pq.pop();
+        int t2=pq.top();
+        pq.pop();
+
+        pq.push(abs(t1-t2));
+       }
+       return pq.top();
+    }
+};
